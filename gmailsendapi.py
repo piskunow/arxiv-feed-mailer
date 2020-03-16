fc5e370215a5be6dcf2093b8c03cba8fe80f234a
Pablo Piskunow <pablo.perez.piskunow@gmail.com>
2020-03-16 15:36:58 +0100

    Use google API latests versions and fix many bugs

--- gmailsendapi.py
+++ gmailsendapi.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-import httplib2
-import os
-from apiclient import discovery
-import oauth2client
-import argparse
+import pickle
+import os.path
 import base64
+
 from email.mime.text import MIMEText
-import sys
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
 from private_variables import my_mail
 
-python_version = sys.version_info.major
-
-flags = argparse.ArgumentParser(
-    parents=[oauth2client.tools.argparser]).parse_args()
-
-SCOPES = 'https://mail.google.com/'
+SCOPES = ['https://www.googleapis.com/auth/gmail.send',
+          ]
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Gmail API Python Quickstart'
 
@@ -24,80 +20,79 @@ APPLICATION_NAME = 'Gmail API Python Quickstart'
 def get_credentials():
     """Gets valid user credentials from storage.
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
     Returns:
         Credentials, the obtained credential.
     """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'mail-script.json')
-
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-
-    if not credentials or credentials.invalid:
-        flow = oauth2client.client.flow_from_clientsecrets(
-            CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = oauth2client.tools.run_flow(flow, store, flags)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
 
 
 def get_service():
     credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('gmail', 'v1', http=http)
+    service = build('gmail', 'v1', credentials=credentials)
     return service
 
 
-def create_message(sender, to, subject, message_text, style='html'):
+def create_message(sender, to, subject, message_text):
     """Create a message for an email.
 
     Args:
-      sender: Email address of the sender.
-      to: Email address of the receiver.
-      subject: The subject of the email message.
-      message_text: The text of the email message.
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
 
     Returns:
-      An object containing a base64url encoded email object.
+        An object containing a base64url encoded email object.
     """
-    if python_version == 2:
-        message_text = unicode(message_text).encode('utf-8')
-    message = MIMEText(message_text, _subtype=style)
-    message['to'] = to
-    message['from'] = sender
-    message['subject'] = subject
-    message = message.as_string()
-    if python_version == 2:
-        message = base64.urlsafe_b64encode(message)
-    else:
-        message = base64.urlsafe_b64encode(
-            message.encode('utf-8')).decode('utf-8')
-    return {'raw': message}
+  message = MIMEText(message_text, 'html')
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  raw = base64.urlsafe_b64encode(message.as_string().encode()).decode()
+  return {'raw': raw}
+
 
 
 def send_message(message):
     """Send an email message.
 
     Args:
-      can be used to indicate the authenticated user.
-      message: Message to be sent.
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
 
     Returns:
-      Sent Message.
+    Sent Message.
     """
     service = get_service()
-    message = service.users().messages().send(userId='me', body=message).execute()
-    print('Message Id: %s' % message['id'])
-    return message
+    user_id = 'me'
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message)
+                   .execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except Exception as error: #urllib2.HTTPError as error:
+        print('An error occurred: %s' % error)
+
 
 
 if __name__ == '__main__':
